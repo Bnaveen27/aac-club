539c2aebb88bcaf196220e440ae56ed8b4d2b028
Bnaveen27 <naveenbala2701@gmail.com>
2025-01-02 12:42:22 +0530

    env fold

--- Server/index.js
+++ Server/index.js
@@ -5,9 +5,15 @@ const InnovatorsModel = require('./models/Innovators')
 const app=express()
 app.use(express.json())
 app.use(cors())
-
-mongoose.connect("mongodb://127.0.0.1:27017/aac");
-//mongodb+srv:naveen:81100@cluster0.3ujvv.mongodb.net/aac?retryWrites=true&w=majority&appName=Cluster0
+require('dotenv').config();
+const uri=process.env.MONGODB_URI;
+// mongoose.connect("mongodb://127.0.0.1:27017/aac");
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database Connected'))
+  .catch(err => console.error('Database connection error:', err));
 
 app.post('/Pages/Login/Login',async(req,res) => {
 
@@ -35,5 +41,5 @@ app.post('/Pages/Registration/Signup',async(req,res)=>{
 
 app.listen(5000,() =>{
     console.log('server is connected')
-    console.log('Database Connected')
+
 })
